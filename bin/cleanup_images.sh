27093f3368ca629b4631e00f0e6838c85ac02c2f
microyzy <microyzy@gmail.com>
2023-04-13 23:26:16 +0900

    up

--- bin/cleanup_images.sh
+++ bin/cleanup_images.sh
@@ -1,8 +1,26 @@
-echo cleaning up  ECR images...
+# this.sh repo_name tag_prefix
+# tag format prefix_datetime
 
-aws ecr list-images --repository-name microyzy
+if [ $# -ne 2 ]; then
+        echo "illegal number of parameters."
+        exit 1
+fi
 
+REPONAME=$1
+TAGPREFIX=$2
+LEN_TAGPREFIX=${#TAGPREFIX}
 
+CURRENT_MONTH="$(date +'%Y%m')"
+echo "Current date: ${CURRENT_MONTH}"
 
-
-echo clean-up done.
+for imagetag in $(aws ecr list-images --repository-name ${REPONAME} --query "imageIds[].[imageTag]" --output text);
+do
+        tagdate="${imagetag:${LEN_TAGPREFIX}:6}"
+        echo "Tag:$imagetag, ${tagdate}"
+        if [ ${#CURRENT_MONTH} -eq ${#tagdate} ] && [ "${CURRENT_MONTH}" -ne "${tagdate}"  ]; then
+                echo "    delete ${imagetag}"
+                aws ecr batch-delete-image --repository-name ${REPONAME} --image-ids imageTag=${imagetag}
+        else
+                echo "    skip ${imagetag}"
+        fi
+done
