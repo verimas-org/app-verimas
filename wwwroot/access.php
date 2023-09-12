0a759ab5ab183c7a1e40ccf4b1d3018ec57e2b7d
microyzy <microyzy@gmail.com>
2023-09-13 00:56:54 +0900

    up

--- wwwroot/access.php
+++ wwwroot/access.php
@@ -9,7 +9,7 @@
                           <hr />
                           <h4>本社所在地</h4>
                           〒103-0022<br>
-                          東京都中央区日本橋室町1丁目11番12号 日本橋水野ビル7階<br>
+                          東京都中央区日本橋室町1丁目11番12号 日本橋水野ビル7階<br>株式会社ヴェリマス<br>
                           <!-- <img src="img/address.png" title="本社所在地"><br> -->
                           </p>
                           <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3240.652323721446!2d139.77320647629824!3d35.68556192973519!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188954295310c1%3A0x33c2e361135a6118!2z44CSMTAzLTAwMjIg5p2x5Lqs6YO95Lit5aSu5Yy65pel5pys5qmL5a6k55S677yR5LiB55uu77yR77yR4oiS77yR77ySIOaXpeacrOapi-awtOmHjuODk-ODqw!5e0!3m2!1sja!2sjp!4v1691642539012!5m2!1sja!2sjp" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
