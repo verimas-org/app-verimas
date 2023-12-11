9f1f323150b506a0df1b15b799e76c1969e1f837
microyzy <microyzy@gmail.com>
2023-12-12 00:45:51 +0900

    update

--- wwwroot/access.php
+++ wwwroot/access.php
@@ -15,12 +15,6 @@
                           <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3240.652323721446!2d139.77320647629824!3d35.68556192973519!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188954295310c1%3A0x33c2e361135a6118!2z44CSMTAzLTAwMjIg5p2x5Lqs6YO95Lit5aSu5Yy65pel5pys5qmL5a6k55S677yR5LiB55uu77yR77yR4oiS77yR77ySIOaXpeacrOapi-awtOmHjuODk-ODqw!5e0!3m2!1sja!2sjp!4v1691642539012!5m2!1sja!2sjp" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                           <p class="mb-5">
                           <hr />
-                          <h4>船橋オフィス（設立予定）</h4>
-                          〒273-0005 千葉県船橋市本町７丁目<br>
-                          </p>
-                          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3239.9950253895763!2d139.98280657631815!3d35.70174002884625!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188078f9cfcb41%3A0x41e25fb2a3af488d!2z6Ii55qmL6aeF!5e0!3m2!1sja!2sjp!4v1690427637016!5m2!1sja!2sjp" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-
-                          <hr />
 
                       </div>
                   </div>
